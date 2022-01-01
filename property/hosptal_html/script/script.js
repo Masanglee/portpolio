e099c680d0c80d3b248c41b40e3c18f161fee83a
Masanglee <qwesse12@naver.com>
2022-01-01 14:26:44 +0900

    Add image

---
+++ property/hosptal_html/script/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('#visual').click(function () {
+        $('.line').fadeOutn(100)
+    })
+    $('.gallery .floor ul').hover(function(){
+        ('.gallery .floor ul li').addClass('big')
+    })
+
+
+})
